75db73a8d0487a628377c99ef749173b15367c33
Mika Chiusiwa <mikachiusiwa@gmail.com>
2025-01-09 10:02:33 +0900

    Add the GameRestart button and button logic

--- script.js
+++ script.js
@@ -70,25 +70,36 @@ const gameBoard = {
             null, null, null
         ],
     currentPlayer: 'X', 
+
+    //Reset the DOM and update the DOM. Iterate with for each to remove the taken class. 
     resetBoard() {
         this.board.fill(null)
+        document.querySelectorAll('.grid-box').forEach(gridbox => {
+            gridbox.textContent = '';
+            gridbox.classList.remove('taken');  //Removes the taken class list to allow players to make moves again
+        });
+        document.getElementById('message-box').textContent = ''
+        this.currentPlayer = 'X'    //Made to ensure X always starts.
+
     },
     
    
     //Logic for making a move on the board
     //Make sure a player 1 cannot override player 2's move and vice versa.
-    makeMove(index){
+    makeMove(index, cell){
         if (this.board[index] === null){
             this.board[index] = this.currentPlayer; // Assigns the index as X if there was nothing in the index
-            this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X'; // Changes the player if the current player is X or O. 
+            cell.textContent = this.currentPlayer
+            cell.classList.add('taken')
+            // Switch Players
+            this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X'; 
         } 
-        this.printBoard()
         this.checkWinner()
     },
 
   //Prints the board on the console 
-  printBoard() {
-    console.log(this.board)
+  printMessage(message) {
+    document.getElementById('message-box') = message;
 },
 
   //All the winning combos
@@ -104,14 +115,14 @@ const gameBoard = {
     ],
 
 
-    //Checks who has won or if there's a tie(NO WINNER) and logs the winner on the console.
-    //An unintended consequence of this is that after X or O wins, the loser will start the next round. 
+    //Checks who has won or if there's a tie(NO WINNER) and logs the winner in the message box.
+    //X will always start the next round
       checkWinner() {
         for(const [a, b, c] of this.winningCombinations) { //iterates over the indices in winning combos
             const [ValueA, ValueB, ValueC] = [this.board[a], this.board[b], this.board[c]] //Using destructing to assign the current values on gameboard indices a, b and c to ValueA, ValueB and ValueC respectively.
             
             if(ValueA && ValueA === ValueB && ValueA === ValueC){ //Checks if all the positions are filled with the same X or O and make sure its not null.
-                console.log(`${ValueA} is the winner!`)
+                this.printMessage(`${ValueA} is the winner!`)
                 this.resetBoard() //Reset the board when the winner is found
                 return;//This is added to STOP the ENTIRE function. 'Break' would be for stopping the current iteration of the loop but allow the function to continue executing
             }
@@ -119,9 +130,12 @@ const gameBoard = {
         
         //Checks if every cell is NOT null(empty) and resets the board after a tie is logged on the console.
         if(this.board.every(cell => cell !== null)) {
-            console.log("It's a tie!")
+            this.printMessage("It's a tie!")
             this.resetBoard();
         }
       } 
     
 };
+
+//DOM Setup
+
